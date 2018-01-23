2824d9f0efb6a270917511fc541c64696964b76f
ramkethireddy2 <ramchander.reddy2@gmail.com>
2018-01-22 22:50:37 -0800

    third commit

--- models/cart.js
+++ models/cart.js
@@ -15,6 +15,16 @@ module.exports = function Cart(oldCart) {
         this.totalPrice += storedItem.item.price;
     };
 
+    this.reduceByOne = function(id) {
+        if (this.items[id] == 0) {
+            this.items[id].qty = 0;
+        }
+        this.items[id].qty--;
+        this.items[id].price -= this.items[id].item.price;
+        this.totalQty--;
+        this.totalPrice -= this.items[id].item.price;
+    }
+
     this.generateArray = function() {
         var arr = [];
         for (var id in this.items) {
